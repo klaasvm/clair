136eb921051207afa66acdb44ae95f053c137878
Klaas Van Mechelen <165936240+klaasvm@users.noreply.github.com>
2024-11-15 17:34:10 +0100

    Upload Files Website

---
+++ config.js
@@ -0,0 +1,60 @@
+// config.js
+
+const config = {
+    groep1: {
+        apiKey: "WHGHDJVGGPOCYW7I",    // Read key
+        channelId: "2688440"           // Channel ID
+    },
+    groep2: {
+        apiKey: "Z37UKTN4HQWYZHM3",    // Read key
+        channelId: "2688427"           // Channel ID
+    },
+    groep3: {
+        apiKey: "RBC45OA94F818R7D",    // Read key
+        channelId: "2688446"           // Channel ID
+    },
+    groep4: {
+        apiKey: "GK16XTJ4REJFKQQK",   // Read key
+        channelId: "2688431"           // Channel ID
+    },
+    groep5: {
+        apiKey: "200PBBPNEK6S4NKX",    // Read key
+        channelId: "2688441"           // Channel ID
+    },
+    groep6: {
+        apiKey: "BNIGA9WNSD9J1EAF",    // Read key
+        channelId: "2688430"           // Channel ID
+    },
+    groep7: {
+        apiKey: "56SKUVU9D5XVROUW",    // Read key
+        channelId: "2688432"           // Channel ID
+    },
+    groep8: {
+        apiKey: "DW97EV1H905BYQZ3",    // Read key
+        channelId: "2688437"           // Channel ID
+    },
+    groep9: {
+        apiKey: "KR4R2A4IO1DOFJ1H",    // Read key
+        channelId: "2688447"           // Channel ID
+    },
+    groep10: {
+        apiKey: "APAG3K0HE84730RS",    // Read key
+        channelId: "2688434"           // Channel ID
+    },
+    groep11: {
+        apiKey: "YZYM1EO2S6S0QN2X",    // Read key
+        channelId: "2688433"           // Channel ID
+    },
+    groep12: {
+        apiKey: "AETRCPAT0M2PPG77",     // Read key
+        channelId: "2688436"           // Channel ID
+    },
+    JosClair1: {
+        apiKey: "258JAVG6IPK5U3ZQ",    // Read key
+        channelId: "2279508"           // Channel ID
+    },
+    JosClair2: {
+        apiKey: "HPEA380O52WNU417",    // Read key
+        channelId: "2215330"           // Channel ID
+    }
+};
